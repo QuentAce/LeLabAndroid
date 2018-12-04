0dca9a7ceb46c15d024ed09fcca78ee1827bcff7
Quentin Chaffois <quentin.chaffois@rmsi.info>
2018-12-04 14:02:59 +0100

    Initialisation projet sur GitHub

---
+++ LeLabAndroid/LeLabAndroid/Views/CountPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LeLabAndroid.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CountPage : ContentPage
+	{
+	    private int _value = 0;
+		public CountPage ()
+		{
+			InitializeComponent ();
+		}
+
+	    private void ButtonAdd(object sender, EventArgs e)
+	    {
+	        _value++;
+	        LabelCount.Text = _value.ToString();
+	    }
+	}
+}
---
+++ LeLabAndroid/LeLabAndroid/Views/MenuPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LeLabAndroid.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MenuPage : ContentPage
+	{
+		public MenuPage ()
+		{
+			InitializeComponent ();
+		}
+
+	    private void CounterButtonCLicked(object sender, EventArgs e)
+	    {
+	        this.Navigation.PushAsync(new CountPage());
+	    }
+	}
+}
